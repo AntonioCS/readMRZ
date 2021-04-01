8246f1c88064d5107608ef369b4495f5bb58bcd6
AntonioCS <antoniocs@gmail.com>
2021-04-01 09:58:34 +0100

    Clean up

--- src/readMRZ.cpp
+++ src/readMRZ.cpp
@@ -2,44 +2,44 @@
 #include <readMRZ/readMRZ.h>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/utility.hpp>
 #include <opencv2/core/mat.hpp>
 #include <tesseract/baseapi.h>
-#include <algorithm>
+
 #include "find_mrz.h"
 
 namespace readMRZ
 {
 	readMRZ::readMRZ(const std::string& tesseractDataPath) : m_tesseractDataPath(tesseractDataPath)
 	{
-		
+		m_ocr.SetVariable("debug_file", "/dev/null"); //suppress all warnings
+
+		m_ocr.Init(!m_tesseractDataPath.empty() ? m_tesseractDataPath.c_str() : nullptr, "ocrb");// , tesseract::OEM_LSTM_ONLY);
+		m_ocr.SetPageSegMode(tesseract::PSM_AUTO);
 	}
 
-	std::string readMRZ::processImage(const std::string& imagePath) const
+	readMRZ::~readMRZ()
 	{
-		cv::Mat imgGrey = cv::imread(imagePath); // , cv::IMREAD_GRAYSCALE);
-		cv::Mat mrz{};
-		std::string result{};
+		m_ocr.End();
+	}
 
-		if (imgGrey.empty()) {
+	std::string readMRZ::processImage(const std::string& imagePath)
+	{
+		cv::Mat img{ cv::imread(imagePath) };
+
+		if (img.empty()) {
 			throw std::runtime_error{"Could not read image: " + imagePath};
 		}
 
-		if (find_mrz(imgGrey, mrz)) {
-			cv::Mat roi_grey;
-			cvtColor(mrz, roi_grey, cv::COLOR_BGR2GRAY);
-			cv::Mat roi_thresh;
-			threshold(roi_grey, roi_thresh, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+		cv::Mat mrz{};
+		std::string result{};
 
-			tesseract::TessBaseAPI ocr{};
-			ocr.Init(!m_tesseractDataPath.empty() ? m_tesseractDataPath.c_str() : nullptr, "eng", tesseract::OEM_LSTM_ONLY);
-			ocr.SetPageSegMode(tesseract::PSM_AUTO);			
-			ocr.SetImage(static_cast<uchar*>(roi_thresh.data), roi_thresh.size().width, roi_thresh.size().height, roi_thresh.channels(), roi_thresh.step1());
-			ocr.Recognize(0);
+		if (find_mrz(img, mrz)) {
+			m_ocr.SetImage(static_cast<uchar*>(mrz.data), mrz.size().width, mrz.size().height, mrz.channels(), mrz.step1());
+			m_ocr.SetSourceResolution(209);
+			m_ocr.Recognize(nullptr);
 
-			result = ocr.GetUTF8Text();
-			ocr.End();
+			result = m_ocr.GetUTF8Text();
 		} else {
 			throw std::runtime_error{ "Could not extract MRZ from image" };
 		}
