2ee38616e673d76b1211c5de302829fac7ff6f32
AntonioCS <antoniocs@gmail.com>
2021-04-01 08:11:06 +0100

    Added more tests

--- tests/src/readMRZTest.cpp
+++ tests/src/readMRZTest.cpp
@@ -10,7 +10,7 @@ TEST_CASE("Read MRZ from images", "[readMRZ]")
 	std::vector<std::pair<std::string, std::string>> testData{
 		{			
 			"test_data/mrz_example_01.jpg",
-			"I0BEL590335801485120100200<<<<8512017F0901015BEL<<<<<<<<<<<7REINARTZ<<ULRIKE<KATIA<E<<<<<<"			
+			"IDBEL590335801485120100200<<<<8512017F0901015BEL<<<<<<<<<<<7REINARTZ<<ULRIKE<KATIA<E<<<<<<"
 		},
 		{			
 			"test_data/mrz_example_02.jpg",
@@ -18,7 +18,23 @@ TEST_CASE("Read MRZ from images", "[readMRZ]")
 		},
 		{			
 			"test_data/mrz_example_03.jpg",
-			"I0HRV000000000000000000000<<<<0000000MO000000HRY<<<<<<<<<<<OUZORAK<<SPECIMEN<<<<<<<<<<<<<<"
+			"IOHRV000000000000000000000<<<<0000000M0000000HRV<<<<<<<<<<<0UZORAK<<SPECIMEN<<<<<<<<<<<<<<"
+		},
+		{
+			"test_data/mrz_example_04.jpg",
+			"IDAUT10000999<6<<<<<<<<<<<<<<<7109094F1112315AUT<<<<<<<<<<<6MUSTERFRAU<<ISOLDE<<<<<<<<<<<<"
+		},
+		{
+			"test_data/mrz_example_05.jpg",
+			"P<GBRJENNINGS<<PAUL<MICHAEL<<<<<<<<<<<<<<<<<0123456784GBR5011025M0810050<<<<<<<<<<<<<<00"
+		},
+		{
+			"test_data/mrz_example_06.jpg",
+			"P<HRVSPECIMEN<<SPECIMEN<<<<<<<<<<<<<<<<<<<<<10070070071HRV8212258F1407019<<<<<<<<<<<<<<06"
+		},
+		{
+			"test_data/mrz_example_07.jpg",
+			"P<KGZLOMAKO<<ALEKSEY<<<<<<<<<<<<<<<<<<<<<<<<AC17494593KGZ8301174M25102952170119831554749"
 		}
 	};
 		
